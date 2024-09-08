9335efe111833e4708874c23ca35a23a5d43a555
ChenKenny <chenkenny0726@gmail.com>
2024-09-08 13:02:25 +0800

    Use Method 2 to sqrt the Matrix

--- UT.m
+++ UT.m
@@ -1,7 +1,8 @@
 function X = UT(x, P, N, lambda)
     X = zeros(N, 2 * N + 1);
     X(:, 1) = x;
-    sqrt_P = sqrtm((N + lambda) * P);
+    %sqrt_P = sqrtm((N + lambda) * P);
+    sqrt_P = matrix_sqrt((N + lambda) * P,2); % Method 2: Cholesky Decomposition
     for i = 1:N
         X(:, i + 1) = x + sqrt_P(:, i);
         X(:, i + N + 1) = x - sqrt_P(:, i);
